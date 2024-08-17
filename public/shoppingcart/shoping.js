e968644c64f80f490cb23258e38f75b05be62bff
Priyanka <shahpriyanka9971@gmail.com>
2024-08-16 23:54:23 -0700

    feat: fetch cards

---
+++ public/shoppingcart/shoping.js
@@ -0,0 +1,113 @@
+
+const shoppingcart= [
+  { 
+    id: 1,
+    img: "/shoppingcart/shiv.png",
+    price: "₹ 20k",
+    tit: "Shiv Shakti",
+    name: "universe",
+    category: "Oil",
+   
+  },
+  { 
+    id: 2,
+    img: "/shoppingcart/daisy.png",
+    price: "₹ 20k",
+    tit: "White Daisy",
+    name: "Shiv",
+    category: "Watercolor",
+
+  },
+  { 
+    id: 3,
+    img: "/shoppingcart/gilhari.png",
+    price: "₹ 20k",
+    tit: "Tired Squirrel",
+    name: "Shiva",
+    category: "Photograph",
+   
+  },
+  { 
+    id: 4,
+    img: "/shoppingcart/lotus.png",
+    price: "₹ 20k",
+    tit: "Lotus",
+    name: "universe",
+    category: "Pastels",
+   
+  },
+  { 
+    id: 5,
+    img: "/shoppingcart/lovers.png",
+    price: "₹ 20k",
+    tit: "Lovers",
+    name: "Unknown",
+    category: "Scluptures",
+   
+  },
+  { 
+    id: 6,
+    img: "/shoppingcart/model.png",
+    price: "₹ 20k",
+    tit: "Birthday Dress",
+    name: "Shiva",
+    category: "Fashion Design",
+   
+  },
+  { 
+    id: 7,
+    img: "/shoppingcart/girl.png",
+    price: "₹ 20k",
+    tit: "Women",
+    name: "shiva",
+    category: "Digital Art",
+    
+  },
+  { 
+    id: 8,
+    img: "/shoppingcart/eye.png",
+    price: "₹ 20k",
+    tit: "An Eye",
+    name: "Max",
+    category: "Digital Art",
+   
+  },
+  { 
+    id: 9,
+    img: "/shoppingcart/pri.png",
+    price: "₹ 20k",
+    tit: "Female",
+    name: "shiva",
+    category: "Portraits",
+    
+  },
+  { 
+    id: 10,
+    img: "/shoppingcart/dancer.png",
+    price: "₹ 20k",
+    tit: "Dance",
+    name: "Shiv",
+    category: "Pastels",
+   
+  },
+  { 
+    id: 11,
+    img: "/shoppingcart/nature.png",
+    price: "₹ 20k",
+    tit: "Forest Lake LandScape  ",
+    name: "Priyanka",
+    category: "Oil",
+    
+  },
+  { 
+    id: 12,
+    img: "/shoppingcart/flower.png",
+    price: "₹ 20k",
+    tit: "Nature",
+    name: "Universe",
+    category: "Photograph",
+   
+  }
+]
+
+export default shoppingcart;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,82 @@
+
+"use client"
+import React, { useState } from 'react';
+import Link from 'next/link';
+const nav = [
+  { id: 1, name: "Art", href:"/pages/art" },
+  { id: 2, name: "Portraits" , href:"/pages/portraits" },
+  { id: 3, name: "Painting" , href:"/pages/painting"},
+  { id: 4, name: "Photography" , href:"/pages/photography" },
+//   { id: 5, name: "Sculptures" , href:"pages/scluptures"},
+//   { id: 6, name: "Fashion Design" , href:"/pages/fashion" },
+//   { id: 7, name: "Digital Art" , href:"/pages/digital" }
+];
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <div className="flex items-center justify-between  p-4 pl-20  lg:pl-20 lg:p-10">
+        {/* <div className=" flex flex-row lg:hidden">
+          <p>Exhibitions</p>
+          <p>Artist</p>
+        </div> */}
+        <div className="lg:hidden">
+          <button onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? (
+              <svg
+                className="w-10 h-10"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="w-10 h-10"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+        <nav className="hidden lg:flex lg:flex-row justify-center items-center lg:gap-20">
+          {nav.map((item) => (
+            <Link key={item.id} href={item.href} className="nav lg:px-2.5">
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+
+      {menuOpen && (
+        <nav className="lg:hidden absolute top-full left-10 w-screen bg-white bg-opacity-20 backdrop-blur-lg shadow-lg border border-white border-opacity-10">
+          {nav.map((item) => (
+            <div key={item.id} className="p-4 border-b border-white border-opacity-20">
+              <Link className="nav" href={item.href}>{item.name}</Link>
+            </div>
+          ))}
+        </nav>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
