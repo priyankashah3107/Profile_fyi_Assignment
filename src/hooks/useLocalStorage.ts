185e7bc1a77950661e73491ef9b9b8447462dec4
Priyanka <shahpriyanka9971@gmail.com>
2024-08-18 06:53:36 -0700

    fix: localStroageIssue

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,22 +1,29 @@
+
 import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-   
     const [value, setValue] = useState<T>(() => {
-        const jsonValue = localStorage.getItem(key)
-
-        if(jsonValue != null) return JSON.parse(jsonValue)
-
-        if(typeof initialValue === "function") {
-            return (initialValue as () => T)()
+        if (typeof window === "undefined") {
+            // If it's on the server, return the initial value
+            return typeof initialValue === "function"
+                ? (initialValue as () => T)()
+                : initialValue;
         } else {
-            return initialValue
+            // If it's on the client, try to get the stored value
+            const jsonValue = localStorage.getItem(key);
+            if (jsonValue != null) return JSON.parse(jsonValue);
+
+            return typeof initialValue === "function"
+                ? (initialValue as () => T)()
+                : initialValue;
         }
-    })
+    });
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value))
-    }, [key, value])
+        if (typeof window !== "undefined") {
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    }, [key, value]);
 
-    return [value, setValue] as [typeof value, typeof setValue]
+    return [value, setValue] as [typeof value, typeof setValue];
 }
